bc57fbf2da76ad98b71fa565f409f875ce03d174
Andrey Novikov <NovikovAA.it@relef.ru>
2021-01-10 16:03:26 +0300

    change app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  template: `
+    <h1>Hello world</h1>
+    <p>My first component</p>
+  `,
+  styles: [`
+    h1, p {
+      color: grey;
+    }
+    p {
+      font-size: 5rem;
+    }
+    `]
 })
 export class AppComponent {
   title = 'Web Developer Blog';
