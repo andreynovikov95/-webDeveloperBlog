3e633472f43db192a08a2d5c57f4d2d5a45cd286
Andrey Novikov <NovikovAA.it@relef.ru>
2021-01-10 16:09:40 +0300

    add new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Hello world</h1>
-    <p>My first component</p>
-  `,
-  styles: [`
-    h1, p {
-      color: grey;
-    }
-    p {
-      font-size: 5rem;
-    }
-    `]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
   title = 'Web Developer Blog';
